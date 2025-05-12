743e0599eb425334581d4d97e5bf2a5aef40830f
Rodrigo Mattos <rodrigo.mattos@ace1.com.br>
2025-05-12 14:50:16 -0300

    fix: product page error

--- server.js
+++ server.js
@@ -4,13 +4,21 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname)));
+
+// Log all requests for debugging
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.url}`);
+  next();
+});
 
 // Handle all routes for Angular HTML5 mode
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  console.log(`Serving index.html for URL: ${req.url}`);
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  console.log(`Open http://localhost:${port}/product/TEN-X500-42-BLK in your browser`);
 }); 
